2c5e71c7ea341c9a5aedd4c21b8d267e7bc493b7
Abhijith M R <abhijithmr581@gmail.com>
2024-06-30 20:35:18 +0530

    Header + Home

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
 
                 <div class='flex max-lg:ml-auto space-x-3'>
                     <button
+                        class='px-2 py-2 text-sm rounded-full font-bold'>CT Code: 8592</button>
+
+                    <button
                         class='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]'>New Session</button>
-                    {/* <button
-                        class='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]'>Sign
-                        up</button> */}
                 </div>
             </div>
         </header>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='pr-11 pl-11'>
+        <div className='md:pr-11 md:pl-11 pl-5 pr-5'>
             <div class="flex flex-col h-screen">
+
+                <div class="flex justify-center items-center h-16 pt-8">
+                    <input type="text" class="border border-gray-300 rounded-lg py-2 px-4 w-full max-w-lg mr-4" placeholder="Type a message..." />
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Send</button>
+                </div>
                 <div class="flex-grow overflow-y-auto">
                     <div class="flex flex-col mb-4 gap-4 py-4">
 
@@ -21,22 +26,13 @@ const Home = () => {
                         <div className='text-right '>
                             <label className='text-sm font-mono font-extrabold'>Laptop</label>
                             <div class="flex justify-end">
-                                <div class="bg-blue-500 rounded-lg px-4 py-2 max-w-[80%] text-left">
-                                    <p class="text-white text-sm">I'm good, thanks! klasfdfff fffkkkkk kkkkkkkkkkk kkkkkkkkkkkkkkk kkkkkkfff ffffffffffffffffff How about you?</p>
+                                <div class="bg-blue-500 rounded-lg px-4 py-2 max-w-[70%] text-left">
+                                    <p class="text-white text-sm">I'm good, thanks! How about you?</p>
                                 </div>
                             </div>
                         </div>
-                        
-                        <div class="flex justify-start">
-                            <div class="bg-gray-100 rounded-lg px-4 py-2 max-w-[80%]">
-                                <p class="text-gray-900 text-sm">I'm doing great, thanks for asking!</p>
-                            </div>
-                        </div>
+
                     </div>
-                </div>
-                <div class="flex justify-center items-center h-16">
-                    <input type="text" class="border border-gray-300 rounded-lg py-2 px-4 w-full max-w-lg mr-4" placeholder="Type a message..." />
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Send</button>
                 </div>
             </div>
         </div>
